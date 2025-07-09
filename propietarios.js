6dc39978fbf9ea59095278caaae4da14985405a7
macsiguidi <macsiguidi@gmail.com>
2025-07-09 13:17:25 -0300

    formulario propietarios

--- propietarios.js
+++ propietarios.js
@@ -48,3 +48,43 @@ setInterval(() => {
 }, 5000);
 
 
+  document.getElementById('formPropietario').addEventListener('submit', function (e) {
+    const fechaHora = document.getElementById('fechaLlamada').value;
+    const hora = new Date(fechaHora).getHours();
+
+    if (hora < 9 || hora >= 18) {
+      e.preventDefault();
+      alert('Por favor seleccioná un horario entre las 09:00 y las 18:00 hs.');
+    }
+  });
+
+
+
+    const formulario = document.getElementById('form-propietario');
+
+  formulario.addEventListener('submit', async function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(formulario);
+    const data = Object.fromEntries(formData.entries());
+
+    try {
+      const response = await fetch('https://disponibilidad-happy-host-patagonia.onrender.com/enviar-formulario', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      if (response.ok) {
+        alert('¡Formulario enviado! Te contactamos pronto 😊');
+        formulario.reset();
+      } else {
+        alert('Error al enviar. Probá de nuevo.');
+      }
+    } catch (error) {
+      console.error('Error al enviar:', error);
+      alert('Error de conexión. Probá más tarde.');
+    }
+  });
